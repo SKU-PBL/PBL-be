4fadf1d20fff24c26c1fca95eff84542a95ad47d
imjuyongp <imjuyongp@gmail.com>
2025-11-01 15:13:11 +0900

    :tada:Start: initial settings

---
+++ src/test/java/com/be/pbl/PblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.pbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PblApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
